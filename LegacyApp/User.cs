6ad27c28100165f69512cdd1531532104d2f163d
Slepy29 <maciejslepowronski@gmail.com>
2024-06-27 22:30:20 +0200

    Upgrade

--- LegacyApp/User.cs
+++ LegacyApp/User.cs
@@ -4,12 +4,49 @@ namespace LegacyApp
 {
     public class User
     {
-        public object Client { get; internal set; }
+        public Client Client { get; internal set; }
         public DateTime DateOfBirth { get; internal set; }
         public string EmailAddress { get; internal set; }
         public string FirstName { get; internal set; }
         public string LastName { get; internal set; }
         public bool HasCreditLimit { get; internal set; }
         public int CreditLimit { get; internal set; }
+
+        public User(Client client, DateTime dateOfBirth, String emailAddress, String firstName, String lastName)
+        {
+            Client = client;
+            DateOfBirth = dateOfBirth;
+            EmailAddress = emailAddress;
+            FirstName = firstName;
+            LastName = lastName;
+            CheckClientType();
+        }
+
+        private void CheckClientType()
+        {
+            switch (Client.Type)
+            {
+                case "VeryImportantClient":
+                    HasCreditLimit = false;
+                    break;
+                case "ImportantClient":
+                    using (var userCreditService = new UserCreditService())
+                    {
+                        int creditLimit = userCreditService.GetCreditLimit(LastName, DateOfBirth);
+                        creditLimit = creditLimit * 2;
+                        CreditLimit = creditLimit;
+                    }
+                    break;
+                default:
+                    HasCreditLimit = true;
+                    using (var userCreditService = new UserCreditService())
+                    {
+                        int creditLimit = userCreditService.GetCreditLimit(LastName, DateOfBirth);
+                        CreditLimit = creditLimit;
+                    }
+                    break;
+            }
+        }
+        
     }
 }
